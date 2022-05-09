902433940c811a0882ebed2e3f6b0969e086c369
BreksamAmr <85411193+BreksamAmr@users.noreply.github.com>
2022-05-09 12:16:37 +0200

    Ya mosahl

---
+++ store/inventory_management/checkout.py
@@ -0,0 +1,5 @@
+from xmlrpc.client import Boolean
+
+
+def checkout(items) -> Boolean:
+    pass
---
+++ store/inventory_management/shop.py
@@ -0,0 +1,12 @@
+class filter_parameters:
+    pass
+
+class item:
+    pass
+
+def search(parameters: filter_parameters) -> list:
+    pass
+
+def getItem(ID: int) -> item:
+    pass
+
